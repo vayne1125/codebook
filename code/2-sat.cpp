7e66252bb4717e67f20d7f6f7670a57deb7f7a24
vayne1125 <vayne20011125@gmail.com>
2022-10-25 11:22:33 +0800

    --2-sat

--- code/2-sat.cpp
+++ code/2-sat.cpp
@@ -3,3 +3,4 @@
 //(a or b) 建立2條邊 (a,b') 和 (b,a') -> (0,3) (1,2)
 //然後跑scc 他的true 或 false 看bln判斷 ans[i] = bln[i] < bln[i+n]
 //然後在用ans的true false去核對條件看對不對 不對就沒有答案
+//轉換技巧 ab' + a'b => ab' + a'b + aa' + bb' => a(a'+b') + b(a'+b') = (a+b)(a'+b')
